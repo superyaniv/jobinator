016620c61beb5b33d7d0f119b1ea5745fc5fb696
Yaniv Alfasy <yaniv@yanivalfasy.com>
2021-02-03 08:27:12 -0500

    adding initial chrome extension data, and example resume json

---
+++ injector.js
@@ -0,0 +1,4 @@
+var placeholder = document.querySelectorAll('.form-control');
+document.querySelector('input[name="Name"]').value = 'Mark Winteringham';
+document.querySelector('input[name="Email"]').value = 'test@email.com';
+document.querySelector('textarea[name="Comment"]').value = 'Lorem ipsum';
