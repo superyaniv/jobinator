60f10906e164a6dca6c345a9c447c281207f0d47
Yaniv Alfasy <yaniv@yanivalfasy.com>
2021-02-03 08:30:01 -0500

    clean up duplicates

--- images/injector.js
+++
@@ -1,4 +0,0 @@
-var placeholder = document.querySelectorAll('.form-control');
-document.querySelector('input[name="Name"]').value = 'Mark Winteringham';
-document.querySelector('input[name="Email"]').value = 'test@email.com';
-document.querySelector('textarea[name="Comment"]').value = 'Lorem ipsum';
